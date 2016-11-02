53dca63d57fd5c8209b0b8f503e62b7d4c153896
piterJ <zoucijun@sina.163>
2016-11-02 17:52:50 +0800

    第二次合并，更新了周二第一次上传的内容

---
+++ app/js/personCenter.js
@@ -0,0 +1,22 @@
+;$(function(){
+	var $person = $('.person');
+	var $head = $person.find('.left');
+	var $my = $('.my');
+	var $wanshanziliao = $my.find('.wanshanziliao');
+	
+	$head.on('singleTap',function(){
+		location.href="../html/myInfo.html";
+	});
+	
+	$wanshanziliao.on('singleTap',function(){
+		location.href="../html/myInfo.html";
+	});
+	
+	//一加载页面就请求localStorage数据，把电话号码放到个人中心
+	var telephone = $person.find('.telephone');
+	var obj =localStorage.getItem('info');
+	console.log(obj);
+	var res =JSON.parse(obj);
+		telephone.html(res.telephone);
+
+})
